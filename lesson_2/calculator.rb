f7df2c4d49dcf46e8dba33b8e5c3bfca6d12e7a6
Ron Jackson <liftheavyobject@outlook.com>
2016-03-01 19:54:51 -0600

    Added Calculator file

---
+++ lesson_2/calculator.rb
@@ -0,0 +1,4 @@
+Kernel.puts("Enter 2 numbers seperated by a space")
+numbers = Kernel.gets().chomp()
+Kernel.puts("Enter an operation: add, subtract, multiply, or divide")
+operation = Kernel.gets().chomp()
