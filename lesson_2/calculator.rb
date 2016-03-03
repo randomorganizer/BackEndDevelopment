d27f101701718dfe583990f89efb583fd9300c13
Ron Jackson <liftheavyobject@outlook.com>
2016-03-02 22:24:48 -0600

    Refactored Calculator w/ Rubocop!

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -1,20 +1,81 @@
 def calculate(a, b, operation)
   case operation
-  when "add" then return a + b
-  when "subtract" then return a - b
-  when "multiply" then return a * b
-  when "divide" then return a.to_f() / b.to_f()
+  when "1" then return a.to_i() + b.to_i()
+  when "2" then return a.to_i() - bto_i()
+  when "3" then return a.to_i() * b.to_i()
+  when "4" then return a.to_f() / b.to_f()
   else puts "Operation not recognized"
   end
 end
 
-Kernel.puts("Enter 2 numbers seperated by a space")
-numbers = Kernel.gets().chomp()
-Kernel.puts("Enter an operation: add, subtract, multiply, or divide")
-operation = Kernel.gets().chomp()
+def prompt(message)
+  Kernel.puts("=> #{message}")
+end
 
-numbers_array = numbers.split(' ')
-value_a = numbers_array[0].to_i()
-value_b = numbers_array[1].to_i()
-result = calculate(value_a, value_b, operation)
-Kernel.puts("When you #{operation} #{value_a} and #{value_b} the result is #{result}")
+def valid_number?(number)
+  number.to_i() != 0
+end
+
+def operation_to_message(operation)
+  case operation
+  when '1'
+    'Adding'
+  when '2'
+    'Subtracting'
+  when '3'
+    'Multiplying'
+  when '4'
+    'Dividing'
+  end
+end
+
+prompt("Welcome to Calculator! Enter your name:")
+
+name = ''
+loop do
+  name = Kernel.gets().chomp()
+  break unless name.empty?() # rubocop told me to use this guard clause
+  prompt("Make sure to use a valid name.")
+end
+prompt("Hi #{name}!")
+
+loop do # main loop
+  first_number = ''
+  loop do
+    prompt("Enter first number")
+    first_number = Kernel.gets().chomp()
+    break if valid_number?(first_number)
+    prompt("Not a valid number")
+  end
+
+  second_number = ''
+  loop do
+    prompt("Enter second number")
+    second_number = Kernel.gets().chomp()
+    break if valid_number?(second_number)
+    prompt("Not a valid number")
+  end
+  operation = ''
+  operator_prompt = <<-MSG
+    Enter an operation:
+    1) Add
+    2) Subtract
+    3) Multiply
+    4) Divide
+  MSG
+  prompt(operator_prompt)
+  loop do
+    operation = Kernel.gets().chomp()
+    break if %w(1 2 3 4).include?(operation)
+    prompt("Must choose 1, 2, 3, or 4")
+  end
+
+  prompt("#{operation_to_message operation} the two numbers...")
+
+  result = calculate(first_number, second_number, operation)
+  prompt("The result is #{result}")
+  prompt("Do you want to preform another calculation? Y/N")
+  answer = Kernel.gets().chomp()
+  break unless answer.downcase().start_with?('y')
+end
+prompt("Thank you for using the Calculator! Good-Bye")
