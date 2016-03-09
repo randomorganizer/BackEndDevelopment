3b80520f746d14679ea96538676c2802052aa03f
Ron Jackson <liftheavyobject@outlook.com>
2016-03-09 15:42:31 -0600

    add tic tac toe walkthrough

---
+++ lesson_4/ttt_walkthrough.rb
@@ -0,0 +1,102 @@
+require 'pry'
+INITIAL_MARKER = ' '.freeze
+PLAYER_MARKER = 'X'.freeze
+COMPUTER_MARKER = 'O'.freeze
+WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] +
+                [[2, 5, 8], [1, 4, 7], [3, 6, 9]] +
+                [[1, 5, 9], [3, 5, 7]]
+def prompt(msg)
+  puts '=> ' + msg
+end
+
+# rubocop: disable Metrics/MethodLength, Metrics/AbcSize
+def display_board(board)
+  system 'clear'
+  puts "You're a #{PLAYER_MARKER}. Computer is an #{COMPUTER_MARKER}"
+  puts ''
+  puts '     |     |'
+  puts "  #{board[1]}  |  #{board[2]}  |  #{board[3]}"
+  puts '     |     |'
+  puts '-----+-----+-----'
+  puts '     |     |'
+  puts "  #{board[4]}  |  #{board[5]}  |  #{board[6]}"
+  puts '     |     |'
+  puts '-----+-----+-----'
+  puts '     |     |'
+  puts "  #{board[7]}  |  #{board[8]}  |  #{board[9]}"
+  puts '     |     |'
+  puts ''
+end
+# rubocop: enable Metrics/MethodLength, Metrics/AbcSize
+
+def initialize_board
+  new_board = {}
+  (1..9).each { |num| new_board[num] = INITIAL_MARKER }
+  new_board
+end
+
+def empty_squares(brd)
+  brd.keys.select { |num| brd[num] == INITIAL_MARKER }
+end
+
+def player_places_piece!(brd)
+  square = ''
+  loop do
+    prompt "Choose a square (#{empty_squares(brd).join(', ')}):"
+    square = gets.chomp.to_i
+    break if empty_squares(brd).include?(square)
+    prompt 'Sorry that is not a valid entry'
+  end
+  brd[square] = PLAYER_MARKER
+end
+
+def computer_places_piece!(brd)
+  square = empty_squares(brd).sample
+  brd[square] = COMPUTER_MARKER
+end
+
+def board_full?(brd)
+  empty_squares(brd).empty?
+end
+
+def someone_won?(brd)
+  detect_winner(brd).nil? ? false : true
+end
+
+def detect_winner(brd)
+  WINNING_LINES.each do |line|
+    if brd.values_at(*line).count(PLAYER_MARKER) == 3
+      return 'Player'
+    elsif brd.values_at(*line).count(COMPUTER_MARKER) == 3
+      return 'Computer'
+    end
+  end
+  nil
+end
+
+loop do
+  board = initialize_board
+  loop do
+    display_board(board)
+
+    player_places_piece!(board)
+    break if someone_won?(board) || board_full?(board)
+
+    computer_places_piece!(board)
+    break if someone_won?(board) || board_full?(board)
+  end
+
+  display_board(board)
+
+  if someone_won?(board)
+    prompt "#{detect_winner(board)} won!"
+  else
+    prompt 'It\'s a tie'
+  end
+
+  prompt 'Play again? (y or n)'
+  answer = gets.chomp.downcase
+  break unless answer.downcase.start_with?('y')
+end
+
+prompt('Thanks for playing Tic Tac Toe! Good-bye!')
