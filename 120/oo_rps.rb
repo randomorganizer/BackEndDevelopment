7b3d22e741c697c48b55d7374ec58a7f852425f0
Ron Jackson <liftheavyobject@outlook.com>
2016-04-02 14:35:48 -0500

    updated to include inheritance for player

--- 120/oo_rps.rb
+++ 120/oo_rps.rb
@@ -1,68 +1,49 @@
 class Player
   attr_accessor :move, :name
 
-  def initialize(player_type = :human)
-    @player_type = player_type
-    @move = nil
+  def initialize
     set_name
   end
+end
 
+class Human < Player
   def set_name
-    if human?
-      n = nil
-      loop do
-        puts "What's your name"
-        n = gets.chomp
-        break unless n.empty?
-        puts "Sorry, must enter a value."
-      end
-      self.name = n
-    else
-      self.name = ['R2D2', 'Hal', 'Chappie', 'Sonny', 'Number 5'].sample
-    end
+    self.name = ['R2D2', 'Hal', 'Chappie', 'Sonny', 'Number 5'].sample
   end
-
   def choose
-    if human?
-      choice = nil
-      loop do
-        puts "Please choose rock, paper, or scissors:"
-        choice = gets.chomp
-        break if ['rock', 'paper', 'scissors'].include? choice
-        puts 'Sorry, invalid choice.'
-      end
-      self.move = choice
-    else
-      self.move = ['rock', 'paper', 'scissors'].sample
+    choice = nil
+    loop do
+      puts "Please choose rock, paper, or scissors:"
+      choice = gets.chomp
+      break if ['rock', 'paper', 'scissors'].include? choice
+      puts 'Sorry, invalid choice.'
     end
-  end
-
-  def human?
-    @player_type == :human
+    self.move = choice
   end
 end
 
-class Move
-  def initialize
-
+class Computer < Player
+  def set_name
+    n = nil
+    loop do
+      puts "What's your name"
+      n = gets.chomp
+      break unless n.empty?
+      puts "Sorry, must enter a value."
+    end
+    self.name = n
   end
-end
-
-class Rule
-  def initialize
-
+  def choose
+    self.move = ['rock', 'paper', 'scissors'].sample
   end
-end
-
-def compare(move1, move2)
 end
 
 class RPSGame
   attr_accessor :human, :computer
 
   def initialize
-    @human = Player.new
-    @computer = Player.new(:computer)
+    @human = Human.new
+    @computer = Computer.new
   end
 
   def display_welcome_message
