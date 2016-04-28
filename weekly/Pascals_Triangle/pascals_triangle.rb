28846bd418ae96af2ab560dbc32ff8a3d29cac3b
Ron Jackson <liftheavyobject@outlook.com>
2016-04-28 08:07:23 -0500

    Added refactored option to Pascals Triangle

--- weekly/Pascals_Triangle/pascals_triangle.rb
+++ weekly/Pascals_Triangle/pascals_triangle.rb
@@ -1,22 +1,21 @@
+# Creates Pascal's Triangle based on number of rows specified
 class Triangle
-
   def initialize(rows)
     @rows = rows
-
   end
 
   def rows
     triangle = []
     @rows.times do
-      triangle << create_row(triangle.last)
+      triangle << create_row2(triangle.last)
     end
     triangle
   end
 
   private
 
-  def create_row(previous_row)
-    return [1] if previous_row == nil
+  def create_row1(previous_row)
+    return [1] if previous_row.nil?
     new_row = [1]
     index_a = 0
     index_b = 1
@@ -28,4 +27,14 @@ class Triangle
     new_row << 1
   end
 
+  def create_row2(previous_row)
+    return [1] if previous_row.nil?
+    new_row = [1]
+    index = [0,1]
+    (1...previous_row.count).inject(index) do |index, _|
+      new_row << (previous_row[index[0]] + previous_row[index[1]])
+      index.map { |i| i + 1 }
+    end
+    new_row << 1
+  end
 end
