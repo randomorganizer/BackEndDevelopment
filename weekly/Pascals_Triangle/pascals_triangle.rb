708acf39dc6b183128877400d73800df88878956
Ron Jackson <liftheavyobject@outlook.com>
2016-04-28 08:11:15 -0500

    Removed alternate method in prep for submission

--- weekly/Pascals_Triangle/pascals_triangle.rb
+++ weekly/Pascals_Triangle/pascals_triangle.rb
@@ -7,14 +7,14 @@ class Triangle
   def rows
     triangle = []
     @rows.times do
-      triangle << create_row2(triangle.last)
+      triangle << create_row(triangle.last)
     end
     triangle
   end
 
   private
 
-  def create_row1(previous_row)
+  def create_row(previous_row)
     return [1] if previous_row.nil?
     new_row = [1]
     index_a = 0
@@ -23,17 +23,6 @@ class Triangle
       new_row << (previous_row[index_a] + previous_row[index_b])
       index_a += 1
       index_b += 1
-    end
-    new_row << 1
-  end
-
-  def create_row2(previous_row)
-    return [1] if previous_row.nil?
-    new_row = [1]
-    index = [0,1]
-    (1...previous_row.count).inject(index) do |index, _|
-      new_row << (previous_row[index[0]] + previous_row[index[1]])
-      index.map { |i| i + 1 }
     end
     new_row << 1
   end
