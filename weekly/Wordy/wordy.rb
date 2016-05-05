bd571f1f3d7ab6c6286efe2a41e74bb344877b06
Ron Jackson <liftheavyobject@outlook.com>
2016-05-05 14:37:03 -0500

    Add weekly challenge Wordy

--- weekly/Wordy/wordy.rb
+++ weekly/Wordy/wordy.rb
@@ -14,19 +14,31 @@ class WordProblem
 
   private
 
+  def parse_question
+    @question.slice!('?')
+    words = @question.split
+    words.each do |word|
+      @values.push(word.to_i) if integer?(word)
+      @operations.push(parse_operator(word)) if operator?(word)
+    end
+  end
+
   def validate_question
     raise ArgumentError, 'No valid values stated' if @values.count == 0
     raise ArgumentError, 'Must provide more than 1 number' if @values.count == 1
     raise ArgumentError, 'Must specify operation' if @operations.count == 0
   end
 
-  def parse_question
-    @question.slice!('?')
-    words = @question.split(' ')
-    words.each do |word|
-      @values.push(word.to_i) if integer?(word)
-      @operations.push(parse_operator(word)) if operator?(word)
+  def calculate
+    total = 0
+    while @values.count > 1
+      value1 = @values.shift
+      value2 = @values.shift
+      operation = @operations.shift
+      total = value1.send operation, value2
+      @values.unshift(total)
     end
+    total
   end
 
   def integer?(text)
@@ -44,15 +56,5 @@ class WordProblem
     return '/' if text.casecmp('divided') == 0
   end
 
-  def calculate
-    total = 0
-    while @values.count > 1
-      value1 = @values.shift
-      value2 = @values.shift
-      operation = @operations.shift
-      total = value1.send operation, value2
-      @values.unshift(total)
-    end
-    total
-  end
+
 end
