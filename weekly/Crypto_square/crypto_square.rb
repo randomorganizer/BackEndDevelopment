e1e5ec3a769fccc100673df22301e160f19b3d08
Ron Jackson <liftheavyobject@outlook.com>
2016-05-09 21:28:07 -0500

    added notes from live session solution

--- weekly/Crypto_square/crypto_square.rb
+++ weekly/Crypto_square/crypto_square.rb
@@ -70,3 +70,49 @@ class Crypto
     @cipher = cipher_square
   end
 end
+
+
+=begin
+  Solution Meeting Notes
+
+  1 normalize the input
+    gsub(/[^a-zA-Z0-9]/, '').downcase
+  2 normalize characters broken into rows
+  3 read down the column from left to right
+
+  If square number then use that
+  If it falls between numbers
+
+  .size
+    normalize_plaintext.size
+    Math.sqrt(plain_text_size).ceil
+
+  .plaintext_segements
+    You could put the text together and then count columns and place a space
+    You need to know what mehtods are available
+    Normalize Plaintext
+    Calculate size
+    chars.each_slice(size).to_a.map(&:join) - good solution
+    arr.group_by (not as good as above)
+    arr.each_with_index.group_by (not as good as above)
+    str.scan(/.{1,#{size}}/) - good solution also
+
+  .ciphertext
+    plaintext_segments
+    array.map{*:chars}
+    transpose method switches rows and columns, but sub arrrays must have same size
+    So you would need to fill arrays with empty strings before transpose
+    chars << '' if chars.size < size
+
+    padded_segements = plaintext_segements.map do |string|
+      string.size < size ? string + ' ' : string
+    end
+    padded_segwments.map(&:chars).trasnspose.map(&:join).map(&:strip).join
+
+  Note in irb _ is the value of the previous return value
+
+
+
+
+ 
+=end
